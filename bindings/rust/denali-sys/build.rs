c19178ffc25352dcfbc5f508f1be22cf7fb0ca4c
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2025-02-20 10:06:26 -0800

    fix (bindings/rust): add c std for nullptr

--- bindings/rust/denali-sys/build.rs
+++ bindings/rust/denali-sys/build.rs
@@ -1,12 +1,12 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 fn main() {
     let src_dir = PathBuf::from("../../../src");
     let include_dir = PathBuf::from("../../../include");
 
-
     cc::Build::new()
         .include(include_dir)
         .file(src_dir.join("display.c"))
+        .std("c23")
         .compile("denali");
 }
