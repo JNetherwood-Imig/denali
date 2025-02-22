6e6c5b7112caf66b5394b40255c9b52615d31499
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2025-02-21 21:11:05 -0800

    style: add printing of connected to match other examples

--- bindings/rust/denali-sys/examples/simple.rs
+++ bindings/rust/denali-sys/examples/simple.rs
@@ -1,11 +1,12 @@
 fn main() {
     unsafe {
-      let display = denali_sys::dwl_display_connect(std::ptr::null_mut());
-      if display.is_null() {
-        eprintln!("Failed to connect to display");
-        return;
-      }
+        let display = denali_sys::dwl_display_connect(std::ptr::null_mut());
+        if display.is_null() {
+            eprintln!("Failed to connect to display");
+            return;
+        }
+        println!("Connected!");
 
-      denali_sys::dwl_display_disconnect(display);
+        denali_sys::dwl_display_disconnect(display);
     }
 }
