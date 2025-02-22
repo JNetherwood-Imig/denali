42628d226e76af93a4307ee6d130c14148ecba46
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2025-02-21 23:12:19 -0800

    chore (examples/simple): run clang-format

--- examples/simple.c
+++ examples/simple.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 int main(void) {
-  DwlDisplay* display = dwl_display_connect(nullptr);
-  if (!display) {
-    printf("Error connecting to display!\n");
-    return -1;  
-  }
-  
-  printf("Connected!\n");
+    DwlDisplay* display = dwl_display_connect(nullptr);
+    if (!display) {
+        printf("Error connecting to display!\n");
+        return -1;
+    }
 
-  dwl_display_disconnect(display);
+    printf("Connected!\n");
+
+    dwl_display_disconnect(display);
 }
